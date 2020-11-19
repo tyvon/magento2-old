2bfb8e3238cb6ff23c701cb09c92b4e3825c43ea
Stas Monarshuk <smonar@softserveinc.com>
2020-11-19 21:45:05 +0200

    Added SoftServe extended theme

---
+++ app/design/frontend/SoftServe/extended/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'frontend/SoftServe/extended',
+    __DIR__
+);
